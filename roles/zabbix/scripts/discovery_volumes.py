daae021f2d53682bfe80ccadc6aec8d8ea7ee6d5
MrCirca <geortsir3@gmail.com>
2017-12-21 20:43:05 +0200

    Add argument, to print volume names

--- roles/zabbix/scripts/discovery_volumes.py
+++ roles/zabbix/scripts/discovery_volumes.py
@@ -2,19 +2,26 @@
 import json
 import subprocess
 import xml.etree.ElementTree as ET
+import sys
 
 gluster_volumes_xml = subprocess.check_output('gluster --xml volume status all detail', shell=True)
 
 #print(gluster_volumes_xml)
 root  = ET.fromstring(gluster_volumes_xml)
 
-volumes = root.find('volStatus').find('volumes').findall('volume')
+def volumeNames():
+    volumes = root.find('volStatus').find('volumes').findall('volume')
+    gluster_volume_info = []
 
-gluster_volume_info = []
+    for volume_name in volumes:
+        gluster_volume_name = volume_name.find('volName').text
+        gluster_volume_info.append({"#VOLUME_NAME": gluster_volume_name})
 
-for volume_name in volumes:
-    gluster_volume_name = volume_name.find('volName').text
-    gluster_volume_info.append({"#VOLUME_NAME": gluster_volume_name})
+    return json.dumps({'data': gluster_volume_info})
 
-print(json.dumps({'data': gluster_volume_info}))
+test = sys.argv[1]
+
+if test == "volumes":
+    print(volumeNames())
+
 
